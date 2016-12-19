e587c8d4a0633ac19c89923976df94a8db3e6d59
Sophie Lucy <sophiejlucy@gmail.com>
2016-12-18 20:37:01 -0500

    string to ast

--- testing/ast.ml
+++ testing/ast.ml
@@ -87,7 +87,7 @@ let string_of_typ = function
   | Void -> "void"
   | String -> "string"
 
-let string_of_vdecl(t,id) = string_of_typ t ^ " " id ^ ";\n"
+let string_of_vdecl(t,id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
 let rec string_of_stmt = function
     Block(stmts) ->
